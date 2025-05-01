a79b8afa0e234bdfdb75236917214190ff0d9eb7
jj-dd <jungin.cha@disruptdigital.com.au>
2025-05-01 23:36:21 +1000

    fix useEffect

--- src/components/Shop/Routes.js
+++ src/components/Shop/Routes.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import ProductItem from "./ProductItem"
 import classes from "./Routes.module.css"
 import { DUMMY_ROUTES } from "../../dummy"
